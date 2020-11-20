546f7e62d9ae6fe83d541065f3242081dfa428c4
vpetters <vpetters@kth.se>
2020-11-20 15:30:17 +0100

    egg edition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class ="logo" alt="Pear logo" src="./assets/pe.png">
+  <HelloWorld msg="The new spotymatch app"/>
 </template>
 
 <script>
@@ -15,6 +15,29 @@ export default {
 </script>
 
 <style>
+@keyframes vibrator {
+    0% {
+      transform: translate(0);
+    }
+    20% {
+      transform: translate(-2px, 2px);
+    }
+    40% {
+      transform: translate(-2px, -2px);
+    }
+    60% {
+      transform: translate(2px, 2px);
+    }
+    80% {
+      transform: translate(2px, -2px);
+    }
+    100% {
+      transform: translate(0);
+    }
+  }
+.logo{
+   animation: vibrator 0.15s linear infinite both;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
