a6b76961f537da671f8955e23517f4ac0616b19e
Johannes Valck <johannesvalck@gmail.com>
2020-11-26 14:28:33 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,22 @@
 <template>
-  <img class ="logo" alt="Pear logo" src="./assets/pe.png">
-  <HelloWorld msg="The new spotymatch app"/>
+
+  <TopMenu />
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopMenu from './components/TopMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopMenu
   }
 }
 </script>
 
+
 <style>
-@keyframes vibrator {
-    0% {
-      transform: translate(0);
-    }
-    20% {
-      transform: translate(-2px, 2px);
-    }
-    40% {
-      transform: translate(-2px, -2px);
-    }
-    60% {
-      transform: translate(2px, 2px);
-    }
-    80% {
-      transform: translate(2px, -2px);
-    }
-    100% {
-      transform: translate(0);
-    }
-  }
-.logo{
-   animation: vibrator 0.15s linear infinite both;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <header id="topMenu">
+        <img class ="logo" src="../assets/pe.png" alt="Pear logo">
+        <h1>Spotymatch</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "TopMenu" 
+}
+</script>
+
+<style scoped>
+    .topMenu {
+        background: #333;
+        color: darkolivegreen;
+        text-align: left;
+        padding: 10px;
+        height: 100px;
+    }
+    /* .logo{
+        height: ;
+    } */
+    h1 {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
+</style>
