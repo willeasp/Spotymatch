f56809bbe42e5f5dd4f1bc54cb5c2b1b1cef0728
Johannes Valck <johannesvalck@gmail.com>
2020-12-09 14:04:54 +0100

    cleanup

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -14,42 +14,42 @@
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="acousticness" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="acousticness" class="slider">
                 <h2>{{"Acousticness: " + acousticness/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="danceability" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="danceability" class="slider">
                 <h2>{{"Danceability: " + danceability/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="energy" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="energy" class="slider" >
                 <h2>{{"Energy: " + energy/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="instrumentalness" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="instrumentalness" class="slider" >
                 <h2>{{"Instrumentalness: " + instrumentalness/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="liveness" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="liveness" class="slider" >
                 <h2>{{"Liveness: " + liveness/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="loudness" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="loudness" class="slider" >
                 <h2>{{"Loudness: " + loudness/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="popularity" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="popularity" class="slider" >
                 <h2>{{"Popularity: " + popularity/1000}}</h2>
             </div>
 
             <div class="slideContainer">
-                <input type="range" min="0" max="1000" v-model="speechiness" class="slider" id="myRange">
+                <input type="range" min="0" max="1000" v-model="speechiness" class="slider" >
                 <h2>{{"Speechiness: " + speechiness/1000}}</h2>
             </div>
             
@@ -59,20 +59,35 @@
                 </div>
             </div>
 
-            <li v-for="track in recommendations" :key="track.name">
-                {{track.name}}
-            </li>
-
         </form>
     </div>
     
 </template>
 
 <script >
+
+
 export default {
     name: "Search",
     data(){
         return {
+            genres:["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", 
+                "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", 
+                "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", 
+                "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", 
+                "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", 
+                "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", 
+                "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", 
+                "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", 
+                "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", 
+                "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", 
+                "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", 
+                "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", 
+                "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", 
+                "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", 
+                "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", 
+                "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", 
+                "trip-hop", "turkish", "work-out", "world-music"],
             acousticness: 500,
             danceability: 500,
             energy: 500,
@@ -89,25 +104,6 @@ export default {
         recommendations() {
             return this.$store.getters.getRecommendations.tracks;
         },
-        genres(){
-            return ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", 
-                "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", 
-                "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", 
-                "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", 
-                "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", 
-                "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", 
-                "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", 
-                "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", 
-                "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", 
-                "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", 
-                "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", 
-                "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", 
-                "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", 
-                "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", 
-                "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", 
-                "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", 
-                "trip-hop", "turkish", "work-out", "world-music"];
-        }
     },
     methods: {
         getToken(){
@@ -138,7 +134,7 @@ export default {
                 this.seedGenres = this.seedGenres.filter(word => word !== genre);
             }
             else{
-                this.seedGenres = this.seedGenres.length < 3 ? [...this.seedGenres, genre] : this.seedGenres;
+                this.seedGenres = this.seedGenres.length < 5 ? [...this.seedGenres, genre] : this.seedGenres;
             }
             // this.seedGenres = this.seedGenres.includes(genre) ? this.seedGenres.filter(genre) : this.seedGenres.push(genre);
         },
@@ -152,11 +148,21 @@ export default {
 <style scoped>
 
 #title{
-    margin: 10% 10% 0 10%;
+    margin: 0 auto;
+    margin-top: 5%;
     text-align: center;
+    background-color: rgba(255, 255, 255, 0.692);
+    width: 300px;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 10px;
+}
+#title:hover{
+    cursor: default;
 }
 form{
-    margin: 10%;
+    margin-left: 10%;
+    margin-right: 10%;
     display: flex;
     flex-direction: column;
 }
@@ -170,7 +176,7 @@ form{
     margin-top: 30px;
     text-align: center;
     transition: .2s;
-    background-color:  rgba(255, 255, 255, 0.507);
+    background-color:  rgba(255, 255, 255, 0.692);
     border-radius: 15px;
     color:black;
     box-shadow: 5px 5px 10px;
@@ -200,11 +206,16 @@ form{
 .genreButton:hover{
     cursor: pointer;
     box-shadow: 5px 5px 5px;
-    color:rgb(27, 133, 50);
+    color: rgba(89, 180, 121, 0.575);
 
+}
+.selected:hover{
+    color: white;
 }
 .selected{
     background-color:  rgb(29, 185, 84);
+    color: white;
+
 }
 
 .slideContainer{
@@ -215,7 +226,7 @@ form{
     margin-top: 30px;
     text-align: center;
     transition: .2s;
-    background-color: rgba(255, 255, 255, 0.507);
+    background-color:  rgba(255, 255, 255, 0.692);
     border-radius: 35px;
     color:black;
     -ms-user-select: none;
@@ -258,6 +269,7 @@ h2{
     margin-top: 70px; 
 }
 #recButton{
+    margin-bottom: 5%;
     height: 50px;
     width: 250px;
     padding: 15px;
