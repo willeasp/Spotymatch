2c21144c93cd0d5fc9ac67baae8e0a274ce8e109
Johannes Valck <johannesvalck@gmail.com>
2020-12-11 17:02:37 +0100

    descriptions added

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -88,59 +88,89 @@
 
 <script >
 
-// const descriptions = {
-//     acousticness: {
-//         name: "Acousticness",
-//         desc: "A confidence measure from 0 to 100% of whether the" 
-//             + "track is acoustic."
-//     },
-//     danceability:{
-//         name: "Danceability",
-//         desc: "Danceability describes how suitable a track is for "
-//             + "dancing based on a combination of musical elements "
-//             + "including tempo, rhythm stability, beat strength, and" 
-//             + " overall regularity."
-//     },
-//     energy: {
-//         name: "Energy",
-//         desc: "Energy represents a perceptual measure of intensity"
-//             + " and activity. Typically, energetic tracks feel fast,"
-//             + " loud, and noisy. For example, death metal has high"
-//             + " energy, while a Bach prelude scores low on the scale. "
-//     },
-//     instrumentalness: {
-//         name: "Instrumentalness",
-//         desc: "Predicts whether a track contains no vocals. “Ooh” and"
-//             + " “aah” sounds are treated as instrumental in this"
-//             + " context. Rap or spoken word tracks are clearly"
-//             + " “vocal”. The closer the instrumentalness value is to"
-//             + " 100%, the greater likelihood the track contains no vocal"
-//             + " content. Values above 50% are intended to represent"
-//             + " instrumental tracks, but confidence is higher as the"
-//             + " value approaches 100%."
-//     },
-//     liveness: {
-//         name: "Liveness",
-//         desc: "Detects the presence of an audience in the recording."
-//             + " Higher liveness values represent an increased"
-//             + " probability that the track was performed live. A value"
-//             + " above 0.8 provides strong likelihood that the track is"
-//             + " live."
-//     },
-//     loudness: {
-//         name: "Loudness",
-//         desc: ""
-//     },
-//     popularity: {
-//         name: "Popularity",
-//     },
-//     speechiness: {
-//         name: "Speechiness",
-//     },
-//     tempo:{
-//         name: "Tempo"
-//     }
-// }
+const descriptions = {
+    acousticness: {
+        name: "Acousticness",
+        desc: "A confidence measure from 0 to 100% of whether the" 
+            + "track is acoustic."
+    },
+    danceability:{
+        name: "Danceability",
+        desc: "Danceability describes how suitable a track is for "
+            + "dancing based on a combination of musical elements "
+            + "including tempo, rhythm stability, beat strength, and" 
+            + " overall regularity."
+    },
+    energy: {
+        name: "Energy",
+        desc: "Energy represents a perceptual measure of intensity"
+            + " and activity. Typically, energetic tracks feel fast,"
+            + " loud, and noisy. For example, death metal has high"
+            + " energy, while a Bach prelude scores low on the scale. "
+    },
+    instrumentalness: {
+        name: "Instrumentalness",
+        desc: "Predicts whether a track contains no vocals. “Ooh” and"
+            + " “aah” sounds are treated as instrumental in this"
+            + " context. Rap or spoken word tracks are clearly"
+            + " “vocal”. The closer the instrumentalness value is to"
+            + " 100%, the greater likelihood the track contains no vocal"
+            + " content. Values above 50% are intended to represent"
+            + " instrumental tracks, but confidence is higher as the"
+            + " value approaches 100%."
+    },
+    liveness: {
+        name: "Liveness",
+        desc: "Detects the presence of an audience in the recording."
+            + " Higher liveness values represent an increased"
+            + " probability that the track was performed live. A value"
+            + " above 0.8 provides strong likelihood that the track is"
+            + " live."
+    },
+    loudness: {
+        name: "Loudness",
+        desc: "he overall loudness of a track in decibels (dB). Loudness"
+            + " values are averaged across the entire track and are"
+            + " useful for comparing relative loudness of tracks."
+            + " Loudness is the quality of a sound that is the primary"
+            + " psychological correlate of physical strength (amplitude)."
+            + " Values typical range between -60 and 0 db."
+    },
+    popularity: {
+        name: "Popularity",
+        desc: "The popularity of the track. The value will be between 0"
+            + " and 100%, with 100% being the most popular. The popularity"
+            + " is calculated by algorithm and is based, in the most part,"
+            + " on the total number of plays the track has had and how"
+            + " recent those plays are. "
+    },
+    speechiness: {
+        name: "Speechiness",
+        desc: "Speechiness detects the presence of spoken words in a track."
+            + " The more exclusively speech-like the recording (e.g. talk"
+            + " show, audio book, poetry), the closer to 100% the attribute"
+            + " value. Values above 66% describe tracks that are probably"
+            + " made entirely of spoken words. Values between 33% and 66%"
+            + " describe tracks that may contain both music and speech,"
+            + " either in sections or layered, including such cases as rap"
+            + " music. Values below 33% most likely represent music and"
+            + " other non-speech-like tracks."
+    },
+    tempo:{
+        name: "Tempo",
+        desc: "The overall estimated tempo of a track in beats per minute"
+            + " (BPM). In musical terminology, tempo is the speed or pace"
+            + " of a given piece and derives directly from the average"
+            + " beat duration."
+    },
+    valence:{
+        name: "Valence",
+        desc: "Describing the musical positiveness conveyed by a track."
+            + " Tracks with high valence sound more positive (e.g. happy,"
+            + " cheerful, euphoric), while tracks with low valence sound"
+            + " more negative (e.g. sad, depressed, angry)."
+    }
+}
 
 export default {
     name: "Search",
