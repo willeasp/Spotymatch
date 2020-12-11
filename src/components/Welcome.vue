c8420908d276acc5d4f39d0caae5ff9a1a63fa47
Simon Johannesson <simonljohannesson@gmail.com>
2020-12-11 09:40:50 +0100

    Reset Welcome.vue after testing

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,18 +1,12 @@
 <template>
-    <!-- <img class="logo" alt="Pear logo" src="../assets/pe.png" />
+    <img class="logo" alt="Pear logo" src="../assets/pe.png" />
     <div class="welcome">
         <h1 id="welcomeText">Welcome to Spotymatch</h1>
         <h2>Route through the page on the topbar</h2>
-    </div> -->
-    <div>
-        <Popup />
-        <button @click="newPopup">Click here to win an iPhone 14!!!!!!</button>
     </div>
 </template>
 
 <script>
-import Popup from './Popup.vue'
-
 export default {
     name: "Welcome",    
     computed: {
@@ -21,14 +15,6 @@ export default {
             return user && user.user;
         },
     },
-    components: {
-        Popup,
-    },
-    methods:{
-        newPopup(){
-            this.$store.dispatch("ADD_MSG", {category: "SURPRISE",msg: "I give you virus!!!!!"})
-        }
-    }
 }
 </script>
 
