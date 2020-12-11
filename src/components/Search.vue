6812a0b947bf10f10a3104b1722868bd314537cf
Johannes Valck <johannesvalck@gmail.com>
2020-12-11 17:33:46 +0100

    description button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -25,6 +25,9 @@
                 v-bind:class="{'disabled':isDisabled(slider.name)}"
                 >
                 <h2 class="sliderTitle">{{slider.name + ": " + Math.round(((slider.value-slider.min)/slider.max)*100)+ "%"}}</h2>
+                <a class="descButton" v-if="isDesc(slider.name)" @click="changeDesc(slider.name)">-</a>
+                <a class="descButton" v-else @click="changeDesc(slider.name)">+</a>
+                <span class="description" v-if="isDesc(slider.name)" >{{slider.desc}}</span>
                 <div class="disableButton" @click="changeDisabled(slider.name)">
                     <span class="disableText" v-if="isDisabled(slider.name)">Enable</span>
                     <span class="disableText" v-else>Disable</span>
@@ -88,90 +91,6 @@
 
 <script >
 
-const descriptions = {
-    acousticness: {
-        name: "Acousticness",
-        desc: "A confidence measure from 0 to 100% of whether the" 
-            + "track is acoustic."
-    },
-    danceability:{
-        name: "Danceability",
-        desc: "Danceability describes how suitable a track is for "
-            + "dancing based on a combination of musical elements "
-            + "including tempo, rhythm stability, beat strength, and" 
-            + " overall regularity."
-    },
-    energy: {
-        name: "Energy",
-        desc: "Energy represents a perceptual measure of intensity"
-            + " and activity. Typically, energetic tracks feel fast,"
-            + " loud, and noisy. For example, death metal has high"
-            + " energy, while a Bach prelude scores low on the scale. "
-    },
-    instrumentalness: {
-        name: "Instrumentalness",
-        desc: "Predicts whether a track contains no vocals. “Ooh” and"
-            + " “aah” sounds are treated as instrumental in this"
-            + " context. Rap or spoken word tracks are clearly"
-            + " “vocal”. The closer the instrumentalness value is to"
-            + " 100%, the greater likelihood the track contains no vocal"
-            + " content. Values above 50% are intended to represent"
-            + " instrumental tracks, but confidence is higher as the"
-            + " value approaches 100%."
-    },
-    liveness: {
-        name: "Liveness",
-        desc: "Detects the presence of an audience in the recording."
-            + " Higher liveness values represent an increased"
-            + " probability that the track was performed live. A value"
-            + " above 0.8 provides strong likelihood that the track is"
-            + " live."
-    },
-    loudness: {
-        name: "Loudness",
-        desc: "he overall loudness of a track in decibels (dB). Loudness"
-            + " values are averaged across the entire track and are"
-            + " useful for comparing relative loudness of tracks."
-            + " Loudness is the quality of a sound that is the primary"
-            + " psychological correlate of physical strength (amplitude)."
-            + " Values typical range between -60 and 0 db."
-    },
-    popularity: {
-        name: "Popularity",
-        desc: "The popularity of the track. The value will be between 0"
-            + " and 100%, with 100% being the most popular. The popularity"
-            + " is calculated by algorithm and is based, in the most part,"
-            + " on the total number of plays the track has had and how"
-            + " recent those plays are. "
-    },
-    speechiness: {
-        name: "Speechiness",
-        desc: "Speechiness detects the presence of spoken words in a track."
-            + " The more exclusively speech-like the recording (e.g. talk"
-            + " show, audio book, poetry), the closer to 100% the attribute"
-            + " value. Values above 66% describe tracks that are probably"
-            + " made entirely of spoken words. Values between 33% and 66%"
-            + " describe tracks that may contain both music and speech,"
-            + " either in sections or layered, including such cases as rap"
-            + " music. Values below 33% most likely represent music and"
-            + " other non-speech-like tracks."
-    },
-    tempo:{
-        name: "Tempo",
-        desc: "The overall estimated tempo of a track in beats per minute"
-            + " (BPM). In musical terminology, tempo is the speed or pace"
-            + " of a given piece and derives directly from the average"
-            + " beat duration."
-    },
-    valence:{
-        name: "Valence",
-        desc: "Describing the musical positiveness conveyed by a track."
-            + " Tracks with high valence sound more positive (e.g. happy,"
-            + " cheerful, euphoric), while tracks with low valence sound"
-            + " more negative (e.g. sad, depressed, angry)."
-    }
-}
-
 export default {
     name: "Search",
 
@@ -201,56 +120,105 @@ export default {
                     name: "acousticness",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "A confidence measure from 0 to 100% of whether the" 
+                        + "track is acoustic."
                 },
                 danceability:{
                     value: 0.5,
                     name: "danceability",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Danceability describes how suitable a track is for "
+                        + "dancing based on a combination of musical elements "
+                        + "including tempo, rhythm stability, beat strength, and" 
+                        + " overall regularity."
                 },
                 energy: {
                     value: 0.5,
                     name: "energy",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Energy represents a perceptual measure of intensity"
+                        + " and activity. Typically, energetic tracks feel fast,"
+                        + " loud, and noisy. For example, death metal has high"
+                        + " energy, while a Bach prelude scores low on the scale. "
                 },
                 instrumentalness: {
                     value: 0.5,
                     name: "instrumentalness",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Predicts whether a track contains no vocals. “Ooh” and"
+                        + " “aah” sounds are treated as instrumental in this"
+                        + " context. Rap or spoken word tracks are clearly"
+                        + " “vocal”. The closer the instrumentalness value is to"
+                        + " 100%, the greater likelihood the track contains no vocal"
+                        + " content. Values above 50% are intended to represent"
+                        + " instrumental tracks, but confidence is higher as the"
+                        + " value approaches 100%."
                 },
                 liveness: {
                     value: 0.5,
                     name: "liveness",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Detects the presence of an audience in the recording."
+                        + " Higher liveness values represent an increased"
+                        + " probability that the track was performed live. A value"
+                        + " above 0.8 provides strong likelihood that the track is"
+                        + " live."
                 },
                 popularity: {
                     value: 50,
                     name: "popularity",
                     min: 0,
                     max: 100,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "The popularity of the track. The value will be between 0"
+                        + " and 100%, with 100% being the most popular. The popularity"
+                        + " is calculated by algorithm and is based, in the most part,"
+                        + " on the total number of plays the track has had and how"
+                        + " recent those plays are. "
                 },
                 speechiness: {
                     value: 0.5,
                     name: "speechiness",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Speechiness detects the presence of spoken words in a track."
+                        + " The more exclusively speech-like the recording (e.g. talk"
+                        + " show, audio book, poetry), the closer to 100% the attribute"
+                        + " value. Values above 66% describe tracks that are probably"
+                        + " made entirely of spoken words. Values between 33% and 66%"
+                        + " describe tracks that may contain both music and speech,"
+                        + " either in sections or layered, including such cases as rap"
+                        + " music. Values below 33% most likely represent music and"
+                        + " other non-speech-like tracks."
                 },
                 valence: {
                     value: 0.5,
                     name: "valence",
                     min: 0,
                     max: 1,
-                    enabled: true
+                    enabled: true,
+                    description: false,
+                    desc: "Describing the musical positiveness conveyed by a track."
+                        + " Tracks with high valence sound more positive (e.g. happy,"
+                        + " cheerful, euphoric), while tracks with low valence sound"
+                        + " more negative (e.g. sad, depressed, angry)."
                 }
             },
             tempo: {
@@ -258,14 +226,21 @@ export default {
                 name: "BPM",
                 min: 10,
                 max: 250,
-                enabled: true
+                enabled: true,
+                desc: ""
             },
             loudness: {
                 value: -30,
                 name: "loudness",
                 min: -60,
                 max: 0,
-                enabled: true
+                enabled: true,
+                desc: "he overall loudness of a track in decibels (dB). Loudness"
+                        + " values are averaged across the entire track and are"
+                        + " useful for comparing relative loudness of tracks."
+                        + " Loudness is the quality of a sound that is the primary"
+                        + " psychological correlate of physical strength (amplitude)."
+                        + " Values typical range between -60 and 0 db."
             },
             seedGenres: [],
         }
@@ -368,6 +343,12 @@ export default {
             else if (slider === this.loudness.name){
                 return !this.loudness.enabled;
             }
+        },
+        changeDesc(slider){
+            this.querySliders[slider].description = !this.querySliders[slider].description;
+        },
+        isDesc(slider){
+            return this.querySliders[slider].description;
         }
     },
 
@@ -546,7 +527,28 @@ form{
             rgb(218, 206, 255) 100%
         );   
 }
-
+.descButton{
+    width: 18px;
+    margin-left: 20px;
+    margin-right: 20px;
+    border-radius: 40px;
+    box-shadow: 1px 1px 3px;
+    text-align: center;
+    background: -webkit-gradient(
+        linear,
+        left top,
+        right bottom,
+        from(#6192ce79),
+        to(#be31a46e)
+        );
+    color: black;
+    font-weight: bold;
+}
+.description{
+    margin-left: 5%;
+    margin-right: 5%;
+    margin-bottom: 5%;
+}
 .disableButton{
     width: 70px;
     padding:5px;
@@ -591,7 +593,7 @@ form{
 .sliderTitle{
     margin-left: 20px;
     margin-top: 5px;
-    width: 100%;
+    width: 85%;
     text-transform: capitalize;
 }
 
