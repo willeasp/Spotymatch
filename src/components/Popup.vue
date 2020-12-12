36294127df75ed224b30956da4483ca37d271658
William Asp <wille.asp@live.se>
2020-12-12 19:11:18 +0100

    ability to choose color (green, red) in popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,8 +6,10 @@
                 v-for="message in getMessages()"
                 :key="message['id']"
                 :msg="message"
-                v-bind:class="{'green': (message['category'] === 'Success!'),
-                                'red': (message['category'] === 'Error')}"
+                v-bind:class="{ 
+                    'green' : (message['category'] === 'Success!'   ||  message['color'] === 'green'),
+                    'red'   : (message['category'] === 'Error'      ||  message['color'] === 'red') 
+                }"
             />
         </transition-group>
     </div>
@@ -45,18 +47,20 @@ export default {
     user-select: none;
     -ms-user-select: none;
     height: 100%;
-    text-shadow: 2px 2px 10px #000000;
+    text-emphasis: before;
 }
 .header {
     margin-top: 5px;
     margin-bottom: 3px;
     font-size: x-large;
+    text-shadow: 1px 1px 8px #000000cb, -1px -1px 5px #00000034;
 }
 .message {
     margin-top: 0px;
     margin-bottom: 5px;
     font-size: medium;
     font-stretch: extra-expanded;
+    text-shadow: 1px 1px 5px #000000, -1px 1px 5px #000000;     /* horizontal, vertical, distance */
 }
 
 .format:hover {
@@ -66,13 +70,15 @@ export default {
     position: fixed;
     width: 100%;
     top: calc(1rem + 30px);
+    z-index: 999;
 }
 /* purgecss end ignore */
 .singleMessage {
     border-radius: 10px;
     margin: 1rem;
-    box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.3);
-    background-color: rgba(255, 255, 255, 0.712);
+    box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.555);
+    background-color: rgba(255, 255, 255, 0.849);
+    backdrop-filter: blur(5px);
 }
 .red {
     background-color: rgba(212, 27, 27, 0.712);
